91278639a6ed9c00ebe11d4accfed0ff77babab9
RCM <132466892+RonMedina-BSIT@users.noreply.github.com>
2025-06-27 20:40:34 +0800

    API

---
+++ BSM_WebApplication/Controllers/BSM_Controller.cs
@@ -0,0 +1,44 @@
+﻿using BSM_BusinessDataLogic;
+using BSM_Common;
+using BSM_DataService;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BSM_WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BSM_Controller : ControllerBase
+    {
+        BSM_DataService.BikeDataServiceForDB bikeDataService = new BSM_DataService.BikeDataServiceForDB();
+        List<Bikeparts> BikeInfo = new List<Bikeparts>();
+
+        [HttpGet]
+        public IEnumerable<Bikeparts> GetAllBikes()
+        {
+            BikeInfo = bikeDataService.GetAllBikes();
+
+            return BikeInfo;
+        }
+        [HttpPost]
+        public bool AddBike(Bikeparts bike)
+        {
+           return bikeDataService.AddBike(bike);
+        }
+
+        [HttpDelete]
+        public bool DeleteBike(Bikeparts bike)
+        {
+ 
+            return bikeDataService.DeleteBike(bike); 
+           
+        }
+        [HttpPatch]
+        public bool UpdateBike(Bikeparts bike)
+        {
+            bikeDataService.UpdateBike(bike);
+            return true;
+        }
+    }
+}
